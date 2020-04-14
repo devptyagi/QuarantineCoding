e555db6e4dd828227c5a8cf935dc5ca6654def20
Dev Tyagi <56474716+devptyagi@users.noreply.github.com>
2020-04-14 18:27:08 +0530

    Update COVIDLQ.java

--- CodeChef/Contest/APRIL20B/COVIDLQ.java
+++ CodeChef/Contest/APRIL20B/COVIDLQ.java
@@ -1,1 +1,33 @@
-// This is an ongoing contest, I will update the code once the contest is over.
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    try{
+    		Scanner sc = new Scanner(System.in);
+    		int t = sc.nextInt();
+    		
+    		while(t-- != 0)
+    		{
+    		    int n = sc.nextInt();
+    		    sc.nextLine();
+    		    String s = sc.nextLine();
+    		    if(s.contains("1 0 1") || s.contains("1 0 0 1") || s.contains("1 0 0 0 1") || s.contains("1 0 0 0 0 1") || s.contains("1 1"))
+    		    {
+    		        System.out.println("NO");
+    		    }
+    		    else
+    		    {
+    		        System.out.println("YES");
+    		    }
+    		}
+	    }
+	    catch(Exception e){
+	        
+	    }
+	}
+}
