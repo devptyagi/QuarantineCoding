1150e894762beed468c8dcdbb05260e8fd2c24e2
Dev Tyagi <56474716+devptyagi@users.noreply.github.com>
2020-04-14 18:28:08 +0530

    Update STRNO.java

--- CodeChef/Contest/APRIL20B/STRNO.java
+++ CodeChef/Contest/APRIL20B/STRNO.java
@@ -1,1 +1,45 @@
-// This is an ongoing contest, I will update the code once the contest is over.
+import java.io.*;
+import java.util.*;
+
+class Solution {
+	public static void main (String[] args) {
+	    try{
+    		Scanner sc = new Scanner(System.in);
+    		int t = sc.nextInt();
+    		
+    		while(t-- != 0)
+    		{
+    		    int n = sc.nextInt();
+    		    int k = sc.nextInt();
+    		    int count = 0;
+    		    
+    		    while(n % 2 == 0)
+    		    {
+    		        n /= 2;
+    		        count++;
+    		    }
+    		    
+    		    for(int i =3; i * i <= n; i++)
+    		    {
+    		        while(n % i == 0)
+    		        {
+    		            n /= i;
+    		            count++;
+    		        }
+    		    }
+    		    
+    		    if(n > 2)
+    		    {
+    		        count++;
+    		    }
+    		    
+    		    if(count < k)
+    		        System.out.println(0);
+    		    else
+    		        System.out.println(1);
+    		}
+	    }catch(Exception e) {
+	        return;
+	    }
+	}
+}
