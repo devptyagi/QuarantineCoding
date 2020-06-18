3e18b83b1ad7aa2355f6897e4053966084360b53
Dev Tyagi <56474716+devptyagi@users.noreply.github.com>
2020-06-18 18:44:10 +0530

    Update EOEO.java

--- CodeChef/Contest/JUNE20B/EOEO.java
+++ CodeChef/Contest/JUNE20B/EOEO.java
@@ -1,1 +1,24 @@
-// This is an Ongoing Contest, I'll update the code once the contest is over.
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while (t-- != 0) {
+		   long ts = sc.nextLong();
+		   if(ts % 2 == 1) {
+		       System.out.println((ts - 1) / 2);
+		   } else {
+		       while(ts % 2 == 0) {
+		           ts = ts / 2;
+		       }
+		       System.out.println((long)ts / 2);
+		   }
+		}
+	}
+}
