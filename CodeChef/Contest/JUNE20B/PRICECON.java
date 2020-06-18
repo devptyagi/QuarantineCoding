562ef173799d3b5922399ff8afca97ea4653e858
Dev Tyagi <56474716+devptyagi@users.noreply.github.com>
2020-06-18 18:42:03 +0530

    Update PRICECON.java

--- CodeChef/Contest/JUNE20B/PRICECON.java
+++ CodeChef/Contest/JUNE20B/PRICECON.java
@@ -1,1 +1,23 @@
-// This is an Ongoing Contest, I'll update the code once the contest is over.
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while (t-- != 0) {
+		   int n = sc.nextInt();
+		   int k = sc.nextInt();
+		   int ans = 0;
+		   for(int i=0;i<n;i++) {
+		       int x = sc.nextInt();
+		       if(x > k) ans += x - k;
+		   }
+		   System.out.println(ans);
+		}
+	}
+}
