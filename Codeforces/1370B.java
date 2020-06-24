21e03e8a96ac477bde4971caf18827f477877047
Dev Tyagi <56474716+devptyagi@users.noreply.github.com>
2020-06-24 17:12:01 +0530

    Create 1370B.java

---
+++ Codeforces/1370B.java
@@ -0,0 +1,184 @@
+import java.util.*;
+import java.io.*;
+
+
+public class B {
+	
+	static class Pair {
+		int x;
+		int y;
+		
+		public Pair(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		public void print() {
+			System.out.println(x+" "+y);
+		}
+		
+	}
+
+	public static void main(String[] args) throws IOException {
+		Reader sc = new Reader();
+		int t = sc.nextInt();
+		while(t-- != 0) {
+			int n = sc.nextInt();
+			ArrayList<Integer> odd = new ArrayList<>();
+			ArrayList<Integer> even = new ArrayList<>();
+			for(int i=1;i<=2*n;i++) {
+				if(sc.nextInt() % 2 == 0) even.add(i);
+				else odd.add(i);
+			}
+			ArrayList<Pair> pairs = new ArrayList<>();
+			for(int i=0;i+1<odd.size();i+=2) {
+				Pair p = new Pair(odd.get(i), odd.get(i+1));
+				pairs.add(p);
+			}
+			for(int i=0;i+1<even.size();i+=2) {
+				Pair p = new Pair(even.get(i), even.get(i+1));
+				pairs.add(p);
+			}
+			for(int i=0;i<n-1;i++) {
+				pairs.get(i).print();
+			}
+		}
+	}
+	
+	static void sort(int[] a) {
+		ArrayList<Integer> l = new ArrayList<>();
+		for(int i : a) l.add(i);
+		Collections.sort(l);
+		for(int i=0; i<a.length; i++) a[i] = l.get(i);
+	}
+	
+	static class Reader { 
+        final private int BUFFER_SIZE = 1 << 16; 
+        private DataInputStream din; 
+        private byte[] buffer; 
+        private int bufferPointer, bytesRead; 
+  
+        public Reader() 
+        { 
+            din = new DataInputStream(System.in); 
+            buffer = new byte[BUFFER_SIZE]; 
+            bufferPointer = bytesRead = 0; 
+        } 
+  
+        public Reader(String file_name) throws IOException 
+        { 
+            din = new DataInputStream(new FileInputStream(file_name)); 
+            buffer = new byte[BUFFER_SIZE]; 
+            bufferPointer = bytesRead = 0; 
+        } 
+  
+        public String readLine() throws IOException 
+        { 
+            byte[] buf = new byte[64]; // line length 
+            int cnt = 0, c; 
+            while ((c = read()) != -1) 
+            { 
+                if (c == '\n') 
+                    break; 
+                buf[cnt++] = (byte) c; 
+            } 
+            return new String(buf, 0, cnt); 
+        } 
+  
+        public int nextInt() throws IOException 
+        { 
+            int ret = 0; 
+            byte c = read(); 
+            while (c <= ' ') 
+                c = read(); 
+            boolean neg = (c == '-'); 
+            if (neg) 
+                c = read(); 
+            do
+            { 
+                ret = ret * 10 + c - '0'; 
+            }  while ((c = read()) >= '0' && c <= '9'); 
+  
+            if (neg) 
+                return -ret; 
+            return ret; 
+        } 
+  
+        public long nextLong() throws IOException 
+        { 
+            long ret = 0; 
+            byte c = read(); 
+            while (c <= ' ') 
+                c = read(); 
+            boolean neg = (c == '-'); 
+            if (neg) 
+                c = read(); 
+            do { 
+                ret = ret * 10 + c - '0'; 
+            } 
+            while ((c = read()) >= '0' && c <= '9'); 
+            if (neg) 
+                return -ret; 
+            return ret; 
+        } 
+  
+        public double nextDouble() throws IOException 
+        { 
+            double ret = 0, div = 1; 
+            byte c = read(); 
+            while (c <= ' ') 
+                c = read(); 
+            boolean neg = (c == '-'); 
+            if (neg) 
+                c = read(); 
+  
+            do { 
+                ret = ret * 10 + c - '0'; 
+            } 
+            while ((c = read()) >= '0' && c <= '9'); 
+  
+            if (c == '.') 
+            { 
+                while ((c = read()) >= '0' && c <= '9') 
+                { 
+                    ret += (c - '0') / (div *= 10); 
+                } 
+            } 
+  
+            if (neg) 
+                return -ret; 
+            return ret; 
+        } 
+  
+        private void fillBuffer() throws IOException 
+        { 
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE); 
+            if (bytesRead == -1) 
+                buffer[0] = -1; 
+        } 
+  
+        private byte read() throws IOException 
+        { 
+            if (bufferPointer == bytesRead) 
+                fillBuffer(); 
+            return buffer[bufferPointer++]; 
+        } 
+  
+        public void close() throws IOException 
+        { 
+            if (din == null) 
+                return; 
+            din.close(); 
+        } 
+        
+        private int[] readArray(int n) throws IOException{
+        	int[] a = new int[n];
+        	for(int i=0;i<n;i++) {
+        		a[i] = nextInt();
+        	}
+        	return a;
+        }
+        
+    }
+
+}
