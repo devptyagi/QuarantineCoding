87745f04f73644f7a59f4461481dfe4c189c31d3
Dev Tyagi <56474716+devptyagi@users.noreply.github.com>
2020-06-26 02:52:12 +0530

    Create 1373B.java

---
+++ Codeforces/1373B.java
@@ -0,0 +1,86 @@
+import java.util.*;
+import java.io.*;
+
+public class B {
+
+	public static void main(String[] args) throws IOException {
+		FastReader sc = new FastReader();
+		int t = sc.nextInt();
+		while(t-- != 0) {
+			char[] s = sc.next().toCharArray();
+			ArrayDeque<Character> stack = new ArrayDeque<>();
+			int turns = 0;
+			for(char c : s) {
+				if(!stack.isEmpty() && stack.peekLast() != c) {
+					stack.removeLast();
+					turns++;
+				}
+				else {
+					stack.addLast(c);
+				}
+			}
+			System.out.println((turns % 2 == 0) ? "NET" : "DA");
+		}
+	}
+	
+	static void sort(int[] a) {
+		ArrayList<Integer> l = new ArrayList<>();
+		for(int i : a) l.add(i);
+		Collections.sort(l);
+		for(int i=0; i<a.length; i++) a[i] = l.get(i);
+	}
+	
+	static class FastReader { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() { 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next() { 
+            while (st == null || !st.hasMoreElements()) { 
+                try { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() { 
+            String str = ""; 
+            try { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+        
+        int[] readArray(int n) {
+        	int[] a = new int[n];
+        	for(int i=0; i<n; i++) {
+        		a[i] = nextInt();
+        	}
+        	return a;
+        }
+        
+    }
+
+}
