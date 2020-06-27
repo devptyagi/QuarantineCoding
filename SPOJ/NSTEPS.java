ad67419751d7ca414fcf55daf8fec00f5cc3b602
Dev Tyagi <56474716+devptyagi@users.noreply.github.com>
2020-06-27 19:48:40 +0530

    Create NSTEPS.java

---
+++ SPOJ/NSTEPS.java
@@ -0,0 +1,88 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+
+	public static void main(String[] args) throws IOException {
+		FastReader sc = new FastReader();
+		OutputStream outputStream = System.out;
+		PrintWriter out = new PrintWriter(outputStream);
+		int t = sc.nextInt();
+		while(t-- != 0) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			if(y == x) {
+				int ans = (x % 2 == 0) ? (2 * x) : (2 * x - 1);
+				out.println(ans);
+			} else if(y == x - 2) {
+				int ans = (x % 2 == 0) ? (2 * x) : (2 * x - 1);
+				ans -= 2;
+				out.println(ans);
+			} else {
+				out.println("No Number");
+			}
+		}
+		out.close();
+	}
+	
+	static void sort(int[] a) {
+		ArrayList<Integer> l = new ArrayList<>();
+		for(int i : a) l.add(i);
+		Collections.sort(l);
+		for(int i=0; i<a.length; i++) a[i] = l.get(i);
+	}
+	
+	static class FastReader { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() { 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next() { 
+            while (st == null || !st.hasMoreElements()) { 
+                try { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() { 
+            String str = ""; 
+            try { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+        
+        int[] readArray(int n) {
+        	int[] a = new int[n];
+        	for(int i=0; i<n; i++) {
+        		a[i] = nextInt();
+        	}
+        	return a;
+        }
+        
+    }
+
+}
