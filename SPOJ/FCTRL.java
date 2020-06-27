1e3f318be1a7dc3f351bbc902584abcdd84271c9
Dev Tyagi <56474716+devptyagi@users.noreply.github.com>
2020-06-27 09:05:15 +0530

    Create FCTRL.java

---
+++ SPOJ/FCTRL.java
@@ -0,0 +1,88 @@
+import java.util.*;
+import java.io.*;
+ 
+class Solution {
+	
+	static int Z(int n) {
+		int z = 0;
+		int d = 5;
+		while(n / d > 0) {
+			z += n/d;
+			d *= 5;
+		}
+		return z;
+	}
+ 
+	public static void main(String[] args) throws IOException {
+		FastReader sc = new FastReader();
+		OutputStream outputStream =System.out;
+                PrintWriter out =new PrintWriter(outputStream);
+		int t = sc.nextInt();
+		while(t-- != 0) {
+			int n = sc.nextInt();
+			out.println(Z(n));
+		}
+		out.close();
+	}
+	
+	static void sort(int[] a) {
+		ArrayList<Integer> l = new ArrayList<>();
+		for(int i : a) l.add(i);
+		Collections.sort(l);
+		for(int i=0; i<a.length; i++) a[i] = l.get(i);
+	}
+	
+	static class FastReader { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() { 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next() { 
+            while (st == null || !st.hasMoreElements()) { 
+                try { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() { 
+            String str = ""; 
+            try { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+        
+        int[] readArray(int n) {
+        	int[] a = new int[n];
+        	for(int i=0; i<n; i++) {
+        		a[i] = nextInt();
+        	}
+        	return a;
+        }
+        
+    }
+ 
+}
