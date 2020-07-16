fe4d42a3ef8592a60a75c0fd9a7cd1e5b981655c
Dev Tyagi <56474716+devptyagi@users.noreply.github.com>
2020-07-16 20:14:52 +0530

    Create CHEFSTR1.java

---
+++ CodeChef/Contest/JULY20B/CHEFSTR1.java
@@ -0,0 +1,88 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+
+	public static void main(String[] args) throws IOException {
+		FastReader sc = new FastReader();
+		OutputStream outputStream = System.out;
+		PrintWriter out = new PrintWriter(outputStream);
+		int t = sc.nextInt();
+		StringBuilder ans = new StringBuilder();
+		while(t-- != 0) {
+			int n = sc.nextInt();
+			int[] a = sc.readArray(n);
+			int cur = a[0];
+			long count = 0;
+			for(int i=1;i<n;i++) {
+				long diff = Math.abs(a[i] - cur);
+				count += diff - 1;
+				cur = a[i];
+			}
+			ans.append(count + "\n");
+		}
+		out.print(ans.toString());
+		out.close();
+	}
+	
+	static void sort(int[] a) {
+		ArrayList<Integer> l = new ArrayList<>();
+		for(int i : a) l.add(i);
+		Collections.sort(l);
+		for(int i=0; i<a.length; i++) a[i] = l.get(i);
+	}
+	
+	static class FastReader { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() { 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next() { 
+            while (st == null || !st.hasMoreElements()) { 
+                try { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() { 
+            String str = ""; 
+            try { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+        
+        int[] readArray(int n) {
+        	int[] a = new int[n];
+        	for(int i=0; i<n; i++) {
+        		a[i] = nextInt();
+        	}
+        	return a;
+        }
+        
+    }
+
+}
