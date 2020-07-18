4390b7f698268c8191696f86ba8be99aea920884
Dev Tyagi <56474716+devptyagi@users.noreply.github.com>
2020-07-18 13:33:38 +0530

    Create 1385B.java

---
+++ Codeforces/1385B.java
@@ -0,0 +1,90 @@
+import java.util.*;
+import java.io.*;
+
+public class B {
+
+	public static void main(String[] args) throws IOException {
+		FastReader sc = new FastReader();
+		OutputStream outputStream = System.out;
+		PrintWriter out = new PrintWriter(outputStream);
+		int t = sc.nextInt();
+		while(t-- != 0) {
+			int n = sc.nextInt();
+			int[] a = sc.readArray(2*n);
+			Set<Integer> set = new HashSet<>();
+			ArrayList<Integer> l = new ArrayList<>();
+			for(int i=0;i<2*n;i++) {
+				if(!set.contains(a[i])) {
+					set.add(a[i]);
+					l.add(a[i]);
+				}
+			}
+			for(int i : l) {
+				out.print(i + " ");
+			}
+			out.println();
+		}
+		out.close();
+	}
+	
+	static void sort(int[] a) {
+		ArrayList<Integer> l = new ArrayList<>();
+		for(int i : a) l.add(i);
+		Collections.sort(l);
+		for(int i=0; i<a.length; i++) a[i] = l.get(i);
+	}
+	
+	static class FastReader { 
+        BufferedReader br; 
+        StringTokenizer st; 
+  
+        public FastReader() { 
+            br = new BufferedReader(new InputStreamReader(System.in)); 
+        } 
+  
+        String next() { 
+            while (st == null || !st.hasMoreElements()) { 
+                try { 
+                    st = new StringTokenizer(br.readLine()); 
+                } 
+                catch (IOException  e) { 
+                    e.printStackTrace(); 
+                } 
+            } 
+            return st.nextToken(); 
+        } 
+  
+        int nextInt() { 
+            return Integer.parseInt(next()); 
+        } 
+  
+        long nextLong() { 
+            return Long.parseLong(next()); 
+        } 
+  
+        double nextDouble() { 
+            return Double.parseDouble(next()); 
+        } 
+  
+        String nextLine() { 
+            String str = ""; 
+            try { 
+                str = br.readLine(); 
+            } 
+            catch (IOException e) { 
+                e.printStackTrace(); 
+            } 
+            return str; 
+        } 
+        
+        int[] readArray(int n) {
+        	int[] a = new int[n];
+        	for(int i=0; i<n; i++) {
+        		a[i] = nextInt();
+        	}
+        	return a;
+        }
+        
+    }
+
+}
